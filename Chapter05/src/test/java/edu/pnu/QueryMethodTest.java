e155fd9e116802679913dec80584910f0bf3f54e
tpfus10 <tpfusdl1a@naver.com>
2024-05-17 13:07:50 +0900

    0517

--- Chapter05/src/test/java/edu/pnu/QueryMethodTest.java
+++ Chapter05/src/test/java/edu/pnu/QueryMethodTest.java
@@ -1,9 +1,7 @@
 package edu.pnu;
 
-import java.util.Date;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,19 +17,6 @@ import edu.pnu.persistence.BoardRepository;
 public class QueryMethodTest {
 	@Autowired
 	private BoardRepository boardRepo;
-
-	@BeforeEach //테스트 메서드 실행 이전에 수행
-	public void dataPrepare() {
-		for(int i = 1; i<=200; i++) {
-			Board board = new Board();
-			board.setTitle("테스트 제목" + i);
-			board.setWriter("테스터");
-			board.setContent("테스트 내용" + i);
-			board.setCreateDate(new Date());
-			board.setCnt(0L);
-			boardRepo.save(board);
-		}
-	}
 	
 	//@Test
 	public void testFindByTitle() {
